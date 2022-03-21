c0c9db9f629b1fc762d23473202bb41c03bc7d11
Andrey Beloboky <andrei.beloboky@yandex.ru>
2022-03-21 23:55:20 +0300

    add implimintation of Iterable in the ArrayList

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Iterator;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList1 ex = new ArrayList1();
+        ex.add("One");
+        ex.add("Two");
+        ex.add("Three");
+        System.out.println(ex.size());
+        Iterator<String> itr = ex.iterator();
+        while (itr.hasNext()) {
+            String value =itr.next();
+            if (value == "Two") {
+                itr.remove();
+            }
+        }
     }
 }
